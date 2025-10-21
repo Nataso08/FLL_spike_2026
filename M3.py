5e2bb7e381cd1a748412ee86ebaaf5b0492aee5e
Toto-exe <deroanto09@gmail.com>
2025-10-21 16:08:47 +0200

    bla bla bla

--- M3.py
+++ M3.py
@@ -10,13 +10,13 @@ def M3 ():
     motorTopL.reset_angle()
     resetImu()
 
-    motorTopR.run_target(1000, 1100, Stop.HOLD, False) #partenza
+    motorTopR.run_target(1000, 1200, Stop.HOLD, False) #partenza
 
     moveToDistance(68, 70, 0, 2.5) #arrivo in missione
     moveToDistance(19, -70, 0, 2.5)
     moveToDistance(9, 40, 0, 2.5)
 
-    motorTopL.run_angle(1100, -1000) #abbocca il "tridente"
+    motorTopL.run_angle(1000, -1000) #abbocca il "tridente"
     wait(200)
     motorTopL.run_angle(1000, 1000)
 
@@ -25,12 +25,12 @@ def M3 ():
     moveToDistance(11, 40, 35, 2.5)
     turnToAngle(0, 20, -45, 2.5)
 
-    motorTopR.run_target(1000, 330) #svolge missione "parco degli spacciatori"
-    moveToDistance(10, 40, -45, 2.5)
+    motorTopR.run_target(1100, 330) #svolge missione "parco degli spacciatori"
+    moveToDistance(9, 35, -38, 2.5)
     motorTopR.run_target(1000, 1200)
     
     moveToDistance(10, -60, -45, 2.5)
-    turnToAngle(0, -30, 20, 2.5)
+    turnToAngle(0, -23, 29, 2.5)
     moveToDistance(80, -80, 20, 2.5) 
 
     print(float(stopwatch.time())/1000, " secondi")
--- M5.py
+++ M5.py
@@ -7,28 +7,26 @@ def M5 ():
     resetImu()
     calibImu(2)
 
-    motorTopL.reset_angle() 
-    motorTopL.run_angle(200, 300, Stop.BRAKE, False) #alzo il braccio
+    motorTopL.run_target(1000, -1450, Stop.BRAKE, False) #alzo il braccio
 
     moveToDistance(73, 60, 0, 2.5, False) 
     moveToDistance(12, 35, 0, 2.5) 
     turnToAngle(19, 0, 60, 2.5)
     moveToDistance(10, 40, 65, 2.5) 
-    turnToAngle(0, -19, 87, 2.5) #allineato con miniera
+    turnToAngle(0, -17, 87, 2.5) #allineato con miniera
 
-
-    #motorTopL.run_target(1000, -150, Stop.BRAKE, True) #abbasso braccio
+    motorTopL.run_target(1000, -250, Stop.BRAKE, True) #abbasso braccio
     moveToDistance(5, 40, 90, 2.5)
-    moveToDistance(15, 55, 90, 2.5)
+    moveToDistance(15, 45, 90, 2.5)#arrivo dentro miniera
+    waitPress()
 
-
-    motorTopL.run_angle(1000, 120)
+    motorTopL.run_target(1000, -850, Stop.BRAKE,True)#"ingroppata"
     moveToDistance(13, -40, 90, 2.5)
     motorTopL.run_target(700, 300, Stop.BRAKE, False)
     turnToAngle(5, 19, 30, 2.5)
     moveToDistance(87, -60, 30, 2.5) 
 
-
+    #print(float(stopwatch.time())/1000, " secondi")
 
 
 M5()
