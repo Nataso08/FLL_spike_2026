0fc189f7458614d5b9a110c16c415ef757bb7f9b
Nataso08 <124895905+Nataso08@users.noreply.github.com>
2025-10-31 08:51:15 +0100

    Udate (30/10/2025)

--- M1.py
+++ M1.py
@@ -13,11 +13,22 @@ def M1 ():
 
     print(hub.battery.voltage())
 
-    moveToDistance(55, 50, 0, 2.5, False)
-    motorTopR.run_target(1000, 400, Stop.HOLD, False)
-    moveToDistance(12, 40, 0, 2.5)
+    moveToDistance(46, 50, 0, 2.5)
+    for i in range(3):
+        motorTopL.run_angle(1000, -150)  #fa uscire i tre smonchi cosi
+        motorTopL.run_angle(1000, 200)
+        wait(200)
+    
+    moveToDistance(3, 50, 0 ,2.5, False)
+    motorTopL.run_angle(1000, -50)
+    moveToDistance(13, 50, 0, 2.5)
+    turnToAngle
+    
+    
+    # motorTopR.run_target(1000, 400, Stop.HOLD, False)
+    # moveToDistance(12, 40, 0, 2.5)
 
-    motorTopR.run_angle(1000, 3000)
+    # motorTopR.run_angle(1000, 3000)
 
 
 
