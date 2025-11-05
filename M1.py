d7df851e7b2fefbd3490e22f14f310a84f77f48c
Toto-exe <deroanto09@gmail.com>
2025-11-05 17:46:25 +0100

    test

--- M1.py
+++ M1.py
@@ -6,41 +6,32 @@ def M1 ():
     stopwatch = StopWatch()
     clockTime = 1000
 
-    #motorTopR.run_angle(600, 1200, Stop.HOLD, False) 
-    
+    #motorTopR.run_angle(1000, 1200, Stop.HOLD, False) 
     
     resetImu()
-    calibImu(2)
+    # calibImu(2)
 
     print(hub.battery.voltage())
 
-    moveToDistance(45, 50, 0, 2.5)
-    #motorTopL.run_target(1500, -400) #Colpi energetici,violenti
-    #motorTopL.run_target(1000, 200)
-
-    #motorTopL.run_target(1500, -400)
-    #motorTopL.run_target(1000, 200)
-
-    #motorTopL.run_target(1500, -400)
-    #motorTopL.run_target(1000, 200)
-
-    #motorTopL.run_target(1500, -400)
-    #motorTopL.run_target(1000, 200)
-
-
-    moveToDistance(30, 40, 0, 2.5)
-
-    turnToAngle(0,13,-33,2.5)
+    moveToDistance(46, 50, 0, 2.5)
+    for i in range(3):
+        motorTopL.run_angle(1000, -150)  #fa uscire i tre smonchi cosi
+        motorTopL.run_angle(1000, 200)
+        wait(200)
     
- 
-
-    moveToDistance(-10,10,0,2.5)
+    moveToDistance(3, 50, 0 ,2.5, False)
+    motorTopL.run_angle(1000, -50)
+    moveToDistance(13, 50, 0, 2.5)
+    turnToAngle
     
-    #motorTopL.run_target(500, -100)
-    #motorTopR.run_angle(700, -700)
-    #motorTopR.run_angle(600, 600)
+    
+    # motorTopR.run_target(1000, 400, Stop.HOLD, False)
+    # moveToDistance(12, 40, 0, 2.5)
 
-    print("M1 - Tempo impiegato: ")
+    # motorTopR.run_angle(1000, 3000)
+
+
+
     print(float(stopwatch.time())/1000, " secondi")
 
 M1()
